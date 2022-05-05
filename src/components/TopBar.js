5a970759eeb9bd26788a45ccdbb9bf2cdf3a92a3
GTM-Winter <dimastriana03@gmail.com>
2022-05-05 20:09:37 +0700

    update topbar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -7,6 +7,7 @@ const TopBar = () => {
         <Container>
           <Navbar.Brand href="/">DimsFilms</Navbar.Brand>
           <Nav>
+            <Nav.Link href="#Tranding">Tranding</Nav.Link>
             <Nav.Link href="#Dracor">Dracor</Nav.Link>
             <Nav.Link href="#Anime">Anime</Nav.Link>
           </Nav>
