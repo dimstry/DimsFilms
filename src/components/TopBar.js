fcd1dd808af2bafb3123154b9ace69b2241124d9
Dimas Triana <dimastriana03@gmail.com>
2022-12-18 13:48:42 +0700

    Update TopBar.js

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -8,7 +8,7 @@ const TopBar = () => {
           <Navbar.Brand href="/">DimsFilms</Navbar.Brand>
           <Nav>
             <Nav.Link href="#Tranding">Tranding</Nav.Link>
-            <Nav.Link href="#Dracor">Dracor</Nav.Link>
+            <Nav.Link href="#Dracor">Drakor</Nav.Link>
             <Nav.Link href="#Anime">Anime</Nav.Link>
           </Nav>
         </Container>
